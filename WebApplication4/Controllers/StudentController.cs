6742bbb35a078d2275ff8bc2fa95135cf039479d
Suman Shrestha <smnxrst39@gmail.com>
2023-04-26 13:29:07 +0545

    lab3

---
+++ WebApplication4/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult StudentView()
+        {
+            StudentModel suman = new StudentModel();
+            suman.FirstName = "Suman";
+            suman.LastName = "Shrestha";
+            suman.Age = 24;
+            suman.Address = "Bafal";
+            suman.CollegeName = "BMC";
+            return View(suman);
+        }
+        public ActionResult StudentsView()
+        {
+            StudentModel suman = new StudentModel();
+            suman.FirstName = "Suman";
+            suman.LastName = "Heiseng";
+            suman.Age = 20;
+            suman.Address = "loas";
+            suman.CollegeName = "BMC";
+            StudentModel jesse = new StudentModel();
+            jesse.FirstName = "jesse    ";
+            jesse.LastName = "PinkMan";
+            jesse.Age = 21;
+            jesse.Address = "Us";
+            jesse.CollegeName = "BMC";
+            StudentModel chandler = new StudentModel();
+            chandler.FirstName = "Chandler";
+            chandler.LastName = "Bing";
+            chandler.Age = 24;
+            chandler.Address = "US";
+            chandler.CollegeName = "BMC";
+            List<StudentModel> Students = new List<StudentModel>();
+            Students.Add(suman);
+            Students.Add(jesse);
+            Students.Add(chandler);
+            return View(Students);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult heisen()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Content()
+        {
+            return Content("This is a content return test by action.");
+        }
+        public ContentResult Contents()
+        {
+            return Content("This is a content return test by ContentResult.");
+        }
+        public JsonResult json()
+        {
+            return Json(new { name = "HEISENG", college = "TUUNI" });
+        }
+        public IActionResult Partial()
+        {
+            return PartialView("heisen"); // layout is discarded
+        }
+        public IActionResult Download()
+        {
+            return File("myfile.txt", "text/plain", "myfile.txt");
+        }
+    }
+}
